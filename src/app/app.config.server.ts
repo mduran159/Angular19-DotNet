a76107ce261f5710aadd63e1825193b55c35d4f8
Maicol Duran <maicoldv@gmail.com>
2025-01-27 12:56:24 -0600

    Angular UI Added

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,11 +3,15 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
+    provideServerRoutesConfig(serverRoutes),
+    provideHttpClient(withFetch()),
+    provideAnimations()
   ]
 };
 
